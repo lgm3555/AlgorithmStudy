3837b33ec9c593cc188a1d4c006d4cd00379f3bf
GreedTy <35190067+GreedTy@users.noreply.github.com>
2020-08-16 17:24:56 +0900

    Add files via upload

---
+++ greedty/level1/1week/Solution2.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+
+        for (int i=0; i<moves.length; i++) {
+            for (int j=0; j<board.length; j++) {
+                if (board[j][moves[i] - 1] != 0) {
+                    arr.add(board[j][moves[i] - 1]);
+                    board[j][moves[i] - 1] = 0;
+                    break;
+                }
+            }
+        }
+
+        for (int k=0; k<arr.size()-1; k++) {
+            if (arr.get(k) == arr.get(k+1)) {
+                answer = answer + 2;
+                arr.remove(k);
+                arr.remove(k);
+                k = -1;
+            }
+        }
+
+        return answer;
+    }
+}
