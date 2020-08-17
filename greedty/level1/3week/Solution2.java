e784d870b30db7dd68cc0d0dcfbfa857d13cb86e
GreedTy <35190067+GreedTy@users.noreply.github.com>
2020-08-17 21:03:08 +0900

    Add files via upload

---
+++ greedty/level1/3week/Solution2.java
@@ -0,0 +1,11 @@
+public class Solution2 {
+    public String solution(String answer) {
+        String testAnswer = "";
+        if (0 == (answer.length() % 2)) {
+            testAnswer = answer.substring(answer.length() / 2 - 1, answer.length() / 2 + 1);
+        } else {
+            testAnswer = answer.substring(answer.length() / 2, answer.length() / 2 + 1);
+        }
+        return testAnswer;
+    }
+}
