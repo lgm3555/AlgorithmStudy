91c2193acd8c3991bc8ea56688e6d0d0fe0e2657
GreedTy <35190067+GreedTy@users.noreply.github.com>
2020-08-30 18:53:51 +0900

    Update and rename Solution3.java to BaseHandlingString.java

--- greedty/level1/4week/BaseHandlingString.java
+++ greedty/level1/4week/BaseHandlingString.java
@@ -1,9 +1,5 @@
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-
-class Solution3 {
-    public static boolean solution1(String s) {
+class Solution {
+    public static boolean solution(String s) {
         if (4 == s.length() || 6 == s.length()) {
             return s.matches("^[0-9]*$");
         } else {
